ceacde7396bdea6bb7c6501e1b3fb037ac7bd701
Thien <thienxuanle@yahoo.com>
2018-12-20 15:31:30 -0500

    Add serive-DAO-Controller for note

--- src/main/java/com/revature/caliber/Application.java
+++ src/main/java/com/revature/caliber/Application.java
@@ -1,13 +1,32 @@
 package com.revature.caliber;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.revature.caliber.beans.Note;
+import com.revature.caliber.dao.NoteRepository;
+
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner {
+	
+	@Autowired
+	private NoteRepository repo;
 	
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
+	}
+
+	/**
+	 * Dummy data
+	 */
+	@Override
+	public void run(String... args) throws Exception {
+		
+		repo.save(new Note(1, "Good job"));
+		repo.save(new Note(2, "Yellow"));
+		
 	}
 
 
