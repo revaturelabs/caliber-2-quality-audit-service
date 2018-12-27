b9dbed676ec6e023bc5aba97562a71db8754e1b9
Phil Gerringer <pgerringer@gmail.com>
2018-12-27 15:10:23 -0500

    added junit test frames

---
+++ src/test/java/com/revature/caliber/AuditServiceDAOTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.revature.caliber;
+
+import junit.framework.TestCase;
+
+/**
+ * @author pgerringer
+ *
+ */
+public class AuditServiceDAOTest extends TestCase {
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+}
