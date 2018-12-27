50142e363d850be1e8b15f474484823719e09b8b
Gerald Roman <44367123+groman-gonzalez@users.noreply.github.com>
2018-12-26 22:20:09 -0500

    Staging (#11)
    * Feature audit table (#2)
    
    * adding feature-audit-table
    
    * Add serive-DAO-Controller for note
    
    * adding necessary bean files (#1)
    
    * adding necessary bean files
    
    * Note.java is Thien's testing file
    
    * Register service (#6)
    
    * adding feature-audit-table
    
    * Add serive-DAO-Controller for note
    
    * adding necessary bean files (#1)
    
    * adding necessary bean files
    
    * Note.java is Thien's testing file
    
    * Notes (#3)
    
    * adding necessary bean files
    
    * Note.java is Thien's testing file
    
    * register client to Eureka server
    
    * added necessary beans
    
    * register service
    
    * register to Zuul
    
    * successfully linked to Zuul server
    
    * commit # 3
    
    * retreive data from user-service
    
    * add BatchController
    
    * Feature audit table (#10)
    
    * adding feature-audit-table
    
    * Add serive-DAO-Controller for note
    
    * adding necessary bean files (#1)
    
    * adding necessary bean files
    
    * Note.java is Thien's testing file
    
    * Notes (#3)
    
    * adding necessary bean files
    
    * Note.java is Thien's testing file
    
    * register client to Eureka server
    
    * added necessary beans
    
    * Added properties file and Dockerfile
    
    * Update Dockerfile
    
    * Update application.yml
    
    * Update application.yml
    
    * Update application.yml
    
    * added h2 and oracle settings

---
+++ src/main/java/com/revature/caliber/controller/NoteController.java
@@ -0,0 +1,138 @@
+package com.revature.caliber.controller;
+
+import java.util.List;
+
+import org.jboss.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.caliber.beans.Note;
+import com.revature.caliber.beans.Trainee;
+import com.revature.caliber.service.NoteService;
+import com.revature.caliber.intercomm.TraineeClient;
+
+/**
+ * Controllers for handling all requests having to do with notes.
+ * @author thienle
+ *
+ */
+@RestController
+@RequestMapping("/note")
+@CrossOrigin(origins="*")
+public class NoteController {
+
+	private static final Logger log = Logger.getLogger(NoteController.class);
+	
+	@Autowired
+	private NoteService service;
+	
+	// Retrieve trainee data from user-service
+	@Autowired
+	private TraineeClient client;
+	
+	/**
+	 * retrieve all trainees from user-service
+	 * @return
+	 */
+	@GetMapping("/trainees")
+	public List<Trainee> findAllTrainees() {
+		return client.findAllTrainees();
+	}
+	
+	/**
+	 * retrieve one trainee from user-service
+	 * @param id
+	 * @return
+	 */
+	@GetMapping("/trainee/{id}")
+	public Trainee findTraineeById(@PathVariable int id) {
+		return client.findTraineeById(id);
+	}
+	
+	/**
+	 * Handles get request for returning all notes
+	 * @return
+	 */
+	@GetMapping("/notes")
+	public List<Note> getAllNotes() {
+		
+		log.trace("in notes: Looking for note");
+		return service.getAllNotes();
+	}
+	
+	/**
+	 * 
+	 * @param id
+	 * @return a note based on noteId
+	 */
+	@GetMapping(value="/note/{id}") 
+	public Note getNote(@PathVariable Integer id) {
+		log.trace("IN FIND ONE NOTE");
+		return service.findById(id);
+	}
+	
+	
+	/**
+	 * Handles post request for creating a note
+	 * @param note 
+	 * @return The created note as well as an OK status code
+	 */
+	@PostMapping(path="/create",
+			     consumes = MediaType.APPLICATION_JSON_VALUE,
+			     produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Note> createNote(@RequestBody Note note) {
+		
+		log.debug("CREATING NOTE: " + note);
+		note = service.createNote(note);
+		
+		if(note == null) {
+			return new ResponseEntity<>(HttpStatus.CONFLICT);
+		} else {
+			return new ResponseEntity<>(note, HttpStatus.CREATED);
+		}
+		
+	}
+	
+	
+	/**
+	 * Updating note
+	 * @param note
+	 * @return
+	 */
+	@PutMapping(path="/update")
+	public ResponseEntity<Note> updateNote(@RequestBody Note note) {
+		log.debug("Updating note: " + note);
+		note = service.updateNote(note);
+		
+
+		if(note == null) {
+			return new ResponseEntity<>(HttpStatus.CONFLICT);
+		} else {
+			return new ResponseEntity<>(note, HttpStatus.CREATED);
+		}
+	}
+	
+	/**
+	 * Delete a note by id
+	 * @param id
+	 * @return
+	 */
+	@DeleteMapping(value="/delete/{id}") 
+	public boolean deleteNote(@PathVariable Integer id) {
+		service.deleteNote(id);
+		return true;
+	}
+	
+	
+}
---
+++ src/main/java/com/revature/caliber/intercomm/BatchClient.java
@@ -0,0 +1,26 @@
+package com.revature.caliber.intercomm;
+
+import java.util.List;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.revature.caliber.beans.Batch;
+import com.revature.caliber.beans.BatchEntity;
+
+@FeignClient("batch-service")
+public interface BatchClient {
+	
+	@GetMapping("/qc/batch/all")
+	//@GetMapping({ "/qc/batch/all", "/vp/batch/all" })
+	public List<Batch> getAllBatches();
+
+	
+	@GetMapping("/all/batch/{id}")
+	public Batch getBatchById(@PathVariable("id") Integer id);
+	
+	
+	@GetMapping("/valid_year")
+	public List<Integer> batchYears();
+}
