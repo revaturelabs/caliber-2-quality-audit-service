860eb7a509a468a38ee16073e7232bf806e31550
Gerald Roman <44367123+groman-gonzalez@users.noreply.github.com>
2018-12-27 14:57:08 -0500

    Feature audit table (#16)
    * adding feature-audit-table
    
    * Add serive-DAO-Controller for note
    
    * adding necessary bean files (#1)
    
    * adding necessary bean files
    
    * Note.java is Thien's testing file
    
    * Notes (#3)
    
    * adding necessary bean files
    
    * Note.java is Thien's testing file
    
    * register client to Eureka server
    
    * added necessary beans
    
    * added h2 and oracle settings
    
    * Register service (#13)
    
    * register service
    
    * register to Zuul
    
    * successfully linked to Zuul server
    
    * commit # 3
    
    * retreive data from user-service
    
    * add BatchController
    
    * #5
    
    * Update application.yml
    
    * Register service (#15)
    
    * register service
    
    * register to Zuul
    
    * successfully linked to Zuul server
    
    * commit # 3
    
    * retreive data from user-service
    
    * add BatchController
    
    * #5
    
    * use url instead of service-id

--- src/main/java/com/revature/caliber/intercomm/TraineeClient.java
+++ src/main/java/com/revature/caliber/intercomm/TraineeClient.java
@@ -8,10 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.revature.caliber.beans.Trainee;
 
-@FeignClient("user-service")
+@FeignClient(name= "user-service", url = "localhost:9085/")
 public interface TraineeClient {
 	
 	/**
@@ -27,4 +28,7 @@ public interface TraineeClient {
 	@PostMapping("/all/trainee/create")
 	public ResponseEntity<Trainee> createTrainee(@RequestBody Trainee trainee);
 	
+	
+//	@GetMapping("all/trainee")
+//	public ResponseEntity<List<Trainee>> findAllByBatch(@RequestParam(required=true) Integer Batch);
 }
