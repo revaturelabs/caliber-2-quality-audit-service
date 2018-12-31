8c00baada58acd6ff59e0cf82a4ceed8c94a360b
Phil Gerringer <pgerringer@gmail.com>
2018-12-31 10:34:06 -0500

    added more dummy data

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -1,2 +1,8 @@
-commit;
+Insert into CALIBER_NOTE (NOTE_ID, NOTE_CONTENT, WEEK_NUMBER, BATCH_ID, TRAINEE_ID, MAX_VISIBILITY, NOTE_TYPE, QC_STATUS) values (1, 'Asked about Hibernate annotations. Answered: @Entity, @Id, @Column', 5, 2003, 1, 'ROLE_QC', 'QC_TRAINEE', 'Good');
+Insert into CALIBER_NOTE (NOTE_ID, NOTE_CONTENT, WEEK_NUMBER, BATCH_ID, TRAINEE_ID, MAX_VISIBILITY, NOTE_TYPE, QC_STATUS) values (2, 'Benefits of Microservices? Answered: Eliminate single point of failure', 5, 2150, 5500, 'ROLE_QC', 'QC_TRAINEE', 'Good');
+Insert into CALIBER_NOTE (NOTE_ID, NOTE_CONTENT, WEEK_NUMBER, BATCH_ID, TRAINEE_ID, MAX_VISIBILITY, NOTE_TYPE, QC_STATUS) values (3, 'Is JavaScript OOP? Answered: Idk. confusing...', 5, 2150, 5501, 'ROLE_QC', 'QC_TRAINEE', 'Good');
+Insert into CALIBER_NOTE (NOTE_ID, NOTE_CONTENT, WEEK_NUMBER, BATCH_ID, TRAINEE_ID, MAX_VISIBILITY, NOTE_TYPE, QC_STATUS) values (4, 'Something smells.  I think it is the trainee', 5, 2150, 5502, 'ROLE_QC', 'QC_TRAINEE', 'Poor');
+Insert into CALIBER_NOTE (NOTE_ID, NOTE_CONTENT, WEEK_NUMBER, BATCH_ID, TRAINEE_ID, MAX_VISIBILITY, NOTE_TYPE, QC_STATUS) values (5, 'Asked questions about collections.  Answered all with confidence.', 5, 2150, 5540, 'ROLE_QC', 'QC_TRAINEE', 'Good');
+Insert into CALIBER_NOTE (NOTE_ID, NOTE_CONTENT, WEEK_NUMBER, BATCH_ID, TRAINEE_ID, MAX_VISIBILITY, NOTE_TYPE, QC_STATUS) values (6, 'Asked about AWS AIM.  correct', 5, 2150, 5457, 'ROLE_QC', 'QC_TRAINEE', 'Good');
 
+commit;
