ea5506aa4367e987bda345e1a13534f55ca192ee
Margy Weathers <mweathers@wisc.edu>
2018-12-31 11:38:01 -0500

    modified h2

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -3,5 +3,5 @@ Insert into CALIBER_NOTE (NOTE_ID, NOTE_CONTENT, WEEK_NUMBER, BATCH_ID, TRAINEE_
 Insert into CALIBER_NOTE (NOTE_ID, NOTE_CONTENT, WEEK_NUMBER, BATCH_ID, TRAINEE_ID, NOTE_TYPE, QC_STATUS) values (3, 'Is JavaScript OOP? Answered: Idk. confusing...', 5, 2150, 5501, 'QC_TRAINEE', 'Good');
 Insert into CALIBER_NOTE (NOTE_ID, NOTE_CONTENT, WEEK_NUMBER, BATCH_ID, TRAINEE_ID, NOTE_TYPE, QC_STATUS) values (4, 'Something smells.  I think it is the trainee', 5, 2150, 5502, 'QC_TRAINEE', 'Poor');
 Insert into CALIBER_NOTE (NOTE_ID, NOTE_CONTENT, WEEK_NUMBER, BATCH_ID, TRAINEE_ID, NOTE_TYPE, QC_STATUS) values (5, 'Asked questions about collections.  Answered all with confidence.', 5, 2150, 5540, 'QC_TRAINEE', 'Good');
-Insert into CALIBER_NOTE (NOTE_ID, NOTE_CONTENT, WEEK_NUMBER, BATCH_ID, TRAINEE_ID, NOTE_TYPE, QC_STATUS) values (6, 'Asked about AWS AIM.  correct', 5, 2150, 5457, 'QC_TRAINEE', 'Good');
+Insert into CALIBER_NOTE (NOTE_ID, NOTE_CONTENT, WEEK_NUMBER, BATCH_ID, TRAINEE_ID, NOTE_TYPE, QC_STATUS) values (6, 'Asked about AWS IAM.  correct', 5, 2150, 5457, 'QC_TRAINEE', 'Good');
 
