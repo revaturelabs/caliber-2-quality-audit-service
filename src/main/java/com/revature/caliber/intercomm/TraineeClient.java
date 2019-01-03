80c7d6b17679b5f5deb35c82439f0a8ca9792d1a
groman-gonzalez <gerald.romangonzalez@gmail.com>
2019-01-03 11:10:22 -0500

    Modified the batch, trainee, and trainer client to have the correct environment variables

--- src/main/java/com/revature/caliber/intercomm/TraineeClient.java
+++ src/main/java/com/revature/caliber/intercomm/TraineeClient.java
@@ -17,10 +17,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.revature.caliber.beans.Trainee;
 
-@FeignClient(name="user", url="localhost:9085")
+//@FeignClient(name="user", url="localhost:9085")
 //@FeignClient(name = "user-service", url = "localhost:9085/")
 //@FeignClient(name= "user-service", url = "http://caliber-v2-1680103809.eu-west-2.elb.amazonaws.com/user")
-//@FeignClient(name = "user-service", url = "${USER_SERVICE_URL}")
+@FeignClient(name = "user-service", url = "${USER_SERVICE_URL}")
 public interface TraineeClient {
 
 	@GetMapping(value = "/all/trainee", produces = MediaType.APPLICATION_JSON_VALUE)
