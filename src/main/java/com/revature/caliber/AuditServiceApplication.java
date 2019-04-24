d57b1ead9cca6d0d3432bc5a0942b543ef34e5a1
Javier Rodriguez <August112641@gmail.com>
2019-04-24 16:10:01 -0400

    Added Config Server Dependency and Bootrap.yml

--- src/main/java/com/revature/caliber/AuditServiceApplication.java
+++ src/main/java/com/revature/caliber/AuditServiceApplication.java
@@ -2,7 +2,7 @@ package com.revature.caliber;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 /**
@@ -13,7 +13,7 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
  *
  */
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableFeignClients
 public class AuditServiceApplication {
 	
