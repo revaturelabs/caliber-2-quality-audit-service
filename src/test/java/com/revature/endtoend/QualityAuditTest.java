da70790f6b456e75f56e93c340d42eec465b0502
SonOfStone <rwbrinson3@gmail.com>
2019-08-10 20:23:40 -0500

    Merged flagging into other tests

--- src/test/java/com/revature/endtoend/QualityAuditTest.java
+++ src/test/java/com/revature/endtoend/QualityAuditTest.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
@@ -29,7 +30,7 @@ public class QualityAuditTest {
 		System.setProperty("webdriver.chrome.driver", "src/test/drivers/chromedriver.exe");
 		
 	    ChromeOptions chromeOptions = new ChromeOptions();
-	    chromeOptions.addArguments("--headless");
+//	    chromeOptions.addArguments("--headless");
 
 	    
 		driver = new ChromeDriver(chromeOptions);
@@ -48,7 +49,7 @@ public class QualityAuditTest {
 		assertEquals(driver.getTitle(), qualityAuditPOM.title);
 	}
 	
-	@Test(dependsOnMethods = { "confirmQualityAuditPage"})
+	@Test(dependsOnMethods = "confirmQualityAuditPage", priority = 7)
 	public void testOverallComments(){
 		
 		WebDriverWait wait = new WebDriverWait(driver, 7);
@@ -74,8 +75,84 @@ public class QualityAuditTest {
 		overallTitleElement = driver.findElements(By.tagName("a"));
 		overallTitleElement.get(0).click();
 	}
+	
+	@Test(dependsOnMethods = "confirmQualityAuditPage", priority = 2)
+	public void testFlagButton() {// this test the functionality of pressing the pencil button for a modal window to pop up
+		WebDriverWait wait = new WebDriverWait(driver, 7);
+		List<WebElement> btnPencilList = driver.findElements(By.className("fa-pen"));
+		List<WebElement> noteRoleList = driver.findElements(By.className("NoteRow"));
+		Actions action = new Actions(driver);
+		action.moveToElement(noteRoleList.get(1)).perform();
+		action.click().build().perform();
+		wait.until(ExpectedConditions.visibilityOf(btnPencilList.get(0)));
+		btnPencilList.get(0).click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("flagTrainee")));
+		assertTrue(driver.findElement(By.id("flagTrainee")).isDisplayed());
+	}
 
-	@Test(priority = 2)
+	@Test(dependsOnMethods = "testFlagButton", priority = 3)
+	public void testGreenBtn() { //tests to see if after click the green flag does the delete,submit,little flag appear
+		WebDriverWait wait = new WebDriverWait(driver, 7);
+		WebElement greenFlag = driver.findElement(By.xpath("//*[@id='greenFlag']"));
+		greenFlag.click();
+		WebElement littleSymbol = driver.findElement(By.xpath("//*[@id='flagTrainee']/div/div/div[1]/div[2]/div/em"));
+		WebElement btnDelete = driver.findElement(By.xpath("//*[@id='flagTrainee']/div/div/div[2]/button[1]"));
+		WebElement btnSubmit = driver.findElement(By.xpath("//*[@id='flagTrainee']/div/div/div[2]/button[2]"));
+		assertTrue(btnDelete.isDisplayed());
+		assertTrue(btnSubmit.isDisplayed());
+		assertTrue(littleSymbol.isDisplayed());
+		assertTrue(littleSymbol.getAttribute("class").equals("fas fa-flag fa-1x fa-green")); //if green flag is clicked little symbol should be green
+		WebElement comment = driver.findElement(By.xpath("//*[@id='comment']"));
+		comment.clear();
+		comment.sendKeys("Hello World");
+		greenFlag.click();
+		btnSubmit.click();
+		WebElement cornerFlag = driver.findElement(By.xpath("//*[@id=\"noTrainees\"]/app-associate/div/table/tbody/tr[2]/td[1]/div/div[1]"));
+		assertTrue(cornerFlag.getAttribute("class").equals("flag-note-area-green dropbtn"));//checks when green flag is clicked then a green corner tag appears
+		Actions action = new Actions(driver);
+		action.moveToElement(cornerFlag).perform();
+		action.build().perform();
+		WebElement flagComment = driver.findElement(By.xpath("//*[@id=\'noTrainees\']/app-associate/div/table/tbody/tr[2]/td[1]/div/div[2]/ul/li"));
+		wait.until(ExpectedConditions.visibilityOf(flagComment));
+		assertTrue(flagComment.isDisplayed());
+		assertTrue(flagComment.getText().equals("Hello World"));
+	}
+
+	@Test(priority = 3)
+	public void testRedBtn(){
+		WebDriverWait wait = new WebDriverWait(driver, 7);
+		List<WebElement> btnPencilList = driver.findElements(By.className("fa-pen"));
+		List<WebElement> noteRoleList = driver.findElements(By.className("NoteRow"));
+		Actions action = new Actions(driver);
+		action.moveToElement(noteRoleList.get(1)).perform();
+		action.click().build().perform();
+		wait.until(ExpectedConditions.visibilityOf(btnPencilList.get(0)));
+		btnPencilList.get(0).click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("flagTrainee")));
+		assertTrue(driver.findElement(By.id("flagTrainee")).isDisplayed());
+
+		WebElement redFlag = driver.findElement(By.xpath("//*[@id=\'redFlag\']"));
+		WebElement littleSymbol = driver.findElement(By.xpath("//*[@id='flagTrainee']/div/div/div[1]/div[2]/div/em"));
+		redFlag.click();
+		WebElement comment = driver.findElement(By.xpath("//*[@id='comment']"));
+		comment.clear();
+		comment.sendKeys("last Test");
+		redFlag.click();
+		WebElement btnSubmit = driver.findElement(By.xpath("//*[@id='flagTrainee']/div/div/div[2]/button[2]"));
+		btnSubmit.click();
+
+		WebElement cornerFlag = driver.findElement(By.xpath("//*[@id=\"noTrainees\"]/app-associate/div/table/tbody/tr[2]/td[1]/div/div[1]"));
+		assertTrue(cornerFlag.getAttribute("class").equals("flag-note-area-red dropbtn"));//checks when red flag is clicked then a red corner tag appears
+		action = new Actions(driver);
+		action.moveToElement(cornerFlag).perform();
+		action.build().perform();
+		WebElement flagComment = driver.findElement(By.xpath("//*[@id=\'noTrainees\']/app-associate/div/table/tbody/tr[2]/td[1]/div/div[2]/ul/li"));
+		wait.until(ExpectedConditions.visibilityOf(flagComment));
+		assertTrue(flagComment.isDisplayed());
+		assertTrue(flagComment.getText().equals("last Test"));
+	}
+
+	@Test(priority = 4)
 	public void testWriteIndividualComments() {
 		QualityAuditPOM qualityAuditPOM = new QualityAuditPOM(driver);
 
@@ -120,7 +197,7 @@ public class QualityAuditTest {
 		assertEquals(true, individualCommentsSaved);
 	}
 
-	@Test(priority = 3)
+	@Test(priority = 5)
 	public void testSortRandomly() {
 		QualityAuditPOM qualityAuditPOM = new QualityAuditPOM(driver);
 
@@ -155,7 +232,7 @@ public class QualityAuditTest {
 		assertEquals(true, differenceFound);
 	}
 
-	@Test(dependsOnMethods = "testSortRandomly")
+	@Test(dependsOnMethods = "testSortRandomly", priority = 6)
 	public void testSortAlphabetically() {
 		QualityAuditPOM qualityAuditPOM = new QualityAuditPOM(driver);
 
