4120cbea7ad819d356652aa24d01d97e526f3ddc
RevatureGentry <william.gentry@revature.com>
2019-08-22 09:50:15 -0400

    FeignClient points to correct URI
    Modified the URI in the CategoryClient to point to the updated endpoint
    in the category-service

--- src/main/java/com/revature/caliber/intercomm/base/CategoryClient.java
+++ src/main/java/com/revature/caliber/intercomm/base/CategoryClient.java
@@ -4,13 +4,14 @@ import com.revature.caliber.beans.Category;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 public interface CategoryClient {
 	
-	@GetMapping(value = "/category/all/active", produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<Category> getAllActiveCategories();
+	@GetMapping(value = "/category/all", produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Category> getAllActiveCategories(@RequestParam(required = false) boolean active);
 	
 	@GetMapping(value = "/category/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public Category getCategoryById(@PathVariable(name = "id") Integer id);
