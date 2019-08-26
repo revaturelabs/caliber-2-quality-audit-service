ed4cf0447301e831a228d7228140ae102b3e809d
RevatureGentry <william.gentry@revature.com>
2019-08-26 10:40:49 -0400

    Updated All Trainers URI
    Modified the URI to get all trainers to match what is presently being
    used

--- src/main/java/com/revature/caliber/intercomm/base/TrainerClient.java
+++ src/main/java/com/revature/caliber/intercomm/base/TrainerClient.java
@@ -13,6 +13,6 @@ import java.util.List;
  */
 public interface TrainerClient {
 
-	@GetMapping("/user/all/trainer/all")
+	@GetMapping("/user/trainers")
 	public ResponseEntity<List<Trainer>> getAllTrainers();
 }
